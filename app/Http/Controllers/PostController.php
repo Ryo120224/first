4d349ae8ccbcdd0bd1a40ed7e0396d920ada47f8
Rho120224 <mryo@stu.kanazawa-u.ac.jp>
2022-09-03 10:16:47 +0900

    first commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,14 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-	public function index(Post $post)
-	{
-		    return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
-	} 
+    //
 }
-?>
--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,7 @@
 |
 */
 
-Route::get('/', 'PostController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/posts/{post}','PostController@show');
