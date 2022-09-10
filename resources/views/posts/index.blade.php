d20e30ed1548e35ebf3f96082f01d65502a46c72
Ryo120224 <mryo@stu.kanazawa-u.ac.jp>
2022-09-10 11:56:57 +0900

    ふたつめコミットメント

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,7 +8,10 @@
     </head>
     <body>
         <h1>Blog Name</h1>
+<<<<<<< HEAD
         <p class='create'>[<a href='/posts/create'>create</a>]</p>
+=======
+>>>>>>> origin/master
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
